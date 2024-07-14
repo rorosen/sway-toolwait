499f349e665fa97c910cae25c7abf4cdf4997f88
Robert Rose <robert.rose@mailbox.org>
2024-07-14 18:07:54 +0200

    spawn sub thread to wait for new window

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use clap::{Args, Parser};
-use std::{thread, time::Duration};
-use swayipc::{Connection, Event, EventType, Fallible, WindowChange};
+use std::{process::ExitCode, sync::mpsc, thread, time::Duration};
+use swayipc::{Connection, Event, EventType, WindowChange};
 
 #[derive(Debug, Parser)]
 struct Cli {
-    #[command(flatten)]
-    check: Check,
-
-    /// Maximum time to wait for a new window
+    /// Number of seconds to wait for a new matching window
     #[arg(short, long, default_value_t = 5)]
     timeout: u64,
 
+    #[command(flatten)]
+    sway: SwayArgs,
+}
+
+#[derive(Debug, Args)]
+// #[group(required = true, multiple = false)]
+struct SwayArgs {
     /// Workspace to run command on
     #[arg(short = 's', long)]
     workspace: u32,
@@ -18,57 +22,64 @@ struct Cli {
     /// Command to run
     #[arg(short, long)]
     command: String,
-}
 
-#[derive(Debug, Args)]
-#[group(required = true, multiple = false)]
-struct Check {
     /// app_id (wayland) or instance string (xwayland) to wait for
     #[arg(short, long)]
-    waitfor: String,
-
-    /// Stop waiting on any new window event (don't check app_id or instance string)
-    #[arg(short, long)]
-    nocheck: bool,
+    waitfor: Option<String>,
 }
 
-fn main() -> Fallible<()> {
+fn main() -> ExitCode {
     let cli = Cli::parse();
+    let (tx, rx) = mpsc::channel();
 
-    thread::spawn(move || {
-        thread::sleep(Duration::from_secs(cli.timeout));
-        eprintln!("no matching new window event within timeout");
-        std::process::exit(1);
-    });
+    thread::spawn(move || tx.send(wait(&cli.sway)));
 
+    match rx.recv_timeout(Duration::from_secs(cli.timeout)) {
+        Ok(Ok(_)) => ExitCode::SUCCESS,
+        Ok(Err(err)) => {
+            eprintln!("Error waiting for matching new window: {err:#}");
+            ExitCode::FAILURE
+        }
+        _ => {
+            eprintln!("Timed out waiting for matching new window");
+            ExitCode::FAILURE
+        }
+    }
+}
+
+fn wait(sway: &SwayArgs) -> Result<(), swayipc::Error> {
     let mut connection = Connection::new()?;
-    let event_stream = Connection::new()?.subscribe([EventType::Window]);
+    let event_stream = Connection::new()?.subscribe([EventType::Window])?;
 
-    connection.run_command(format!("workspace {}", cli.workspace))?;
-    connection.run_command(format!("exec {}", cli.command))?;
+    connection.run_command(format!("workspace {}", sway.workspace))?;
+    connection.run_command(format!("exec {}", sway.command))?;
 
-    for event in event_stream? {
-        if let Event::Window(window_event) = event? {
-            if window_event.change == WindowChange::New {
-                if cli.check.nocheck {
-                    return Ok(());
-                } else {
-                    if let Some(app_id) = window_event.container.app_id {
-                        if app_id == cli.check.waitfor {
-                            return Ok(());
-                        }
-                    } else if let Some(properties) = window_event.container.window_properties {
-                        if let Some(instance) = properties.instance {
-                            if instance == cli.check.waitfor {
+    for event in event_stream {
+        match event? {
+            Event::Window(ev) if ev.change == WindowChange::New => {
+                match &sway.waitfor {
+                    Some(waitfor) => {
+                        // TODO: chain once if-let-chains are stable https://github.com/rust-lang/rust/issues/53667
+                        if let Some(app_id) = &ev.container.app_id {
+                            if app_id == waitfor {
                                 return Ok(());
                             }
                         }
+
+                        if let Some(properties) = &ev.container.window_properties {
+                            if let Some(instance) = &properties.instance {
+                                if instance == waitfor {
+                                    return Ok(());
+                                }
+                            }
+                        }
                     }
-                }
+                    None => return Ok(()),
+                };
             }
+            _ => (),
         }
     }
 
-    eprintln!("stopped to receive window events");
-    std::process::exit(1);
+    panic!("Stopped to receive sway/i3 window events");
 }
